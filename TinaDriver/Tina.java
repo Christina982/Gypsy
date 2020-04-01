c65f310820c640602d222f80efe287332725b41b
tina martinez <clmartinez29@yahoo.com>
2020-03-31 19:35:20 -0700

    first commit

---
+++ TinaDriver/Tina.java
@@ -0,0 +1,32 @@
+
+/**
+ * Write a description of class TinaDriver here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Tina
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    
+    public Tina()
+    {
+        // initialise instance variables
+        x = 7;
+    }
+
+    
+    
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+    
+    public void sayHello()
+    {
+        System.out.println("hello world");
+    }
+}
---
+++ TinaDriver/TinaMain.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class TinaMain here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class TinaMain
+{
+    public static void main(String[] args)
+    {
+        Tina tina = new Tina();
+        tina.sayHello();
+        System.out.println("5+x=" + tina.sampleMethod(5));
+    }
+}
