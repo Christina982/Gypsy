d0a26a2cd0983602d58826738eb911c0960645b5
Jason Martinez <jmartinez059@yahoo.com>
2020-04-05 14:06:53 -0500

    Repo has a skeleton class Cocktail in AdultBeverages

---
+++ AdultBeverages/Cocktial.java
@@ -0,0 +1,12 @@
+/**
+ * This class extends AdultBeverages and describes a Cocktail Drink
+ *
+ * @author Jason Martinez
+ * @version 1.0
+ * @since April 5, 2020
+ */
+
+public class Cocktail extends AdultBeverage {
+	
+
+}
