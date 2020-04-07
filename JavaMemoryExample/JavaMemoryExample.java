d8c5578f30af366a93e58ea232d20624f3863d0a
Jason Martinez <jmartinez059@yahoo.com>
2020-04-07 07:49:33 -0500

    JavaMemeoryExample.java shows memeory allocation for arrays

--- JavaMemoryExample/JavaMemoryExample.java
+++ JavaMemoryExample/JavaMemoryExample.java
@@ -13,33 +13,29 @@ public class JavaMemoryExample {
 		String string2;
 		Tina tina1;
 		/*
-			______
-			|Wine  |  
-			|______|
-			|tina1 |
-			|______|
-			|0x2344|
-			|______|
-			______
-			|Wine|  
-			|______|
-			|string2|
-			|______|
-			|0x2343|
-			|______|
-			 _____
-			|AdultBev|  
-			|______|
-			|string1|
-			|______|
-			|0x2343  |
-			|______|
+			Stack									Heap								_________________________
+																					   |         <Type>          |
+			 ___________															   |_ _ _ _ _ _ _ _ _ _ _ _ _|
+			|Tina       |	  						0x2342 : "some string"			   |         <Name>          |
+			|_ _ _ _ _ _|							0x2343 : "have a nice day"		   |_ _ _ _ _ _ _ _ _ _ _ _ _|
+			|tina1      |	  						0x2344 : Tina					   |<pointer/primitive value>|
+			|_ _ _ _ _ _|															   |_________________________|
+			|0x2344     |
+			|___________|
+			|String     |	  
+			|_ _ _ _ _ _|
+			|strings    |	  
+			|_ _ _ _ _ _|
+			|0x2343     |
+			|___________|
+			|String     |	  
+			|_ _ _ _ _ _|
+			|string1    |	  
+			|_ _ _ _ _ _|
+			|0x2343     |
+			|___________|			
 			
-			0x2342 : "some string"
-			0x2343 : "have a nice day"
-			0x2344 : Tina
-			
-*			*/
+			*/
 
 	
 		string1 = "some string";
@@ -49,8 +45,93 @@ public class JavaMemoryExample {
 		string1 = string2;
 		string2 = string1; 			// wont work
 		System.out.prinln(string1);		// output: "have a nice day"
-		string1 = tina1;
+		string1 = tina1; 				// wont work
+		
+		
 		AdultBeverages ab = new Wine();
 		Wine wine = new AdultBeverages();  //wont work
+		Wine wine2 = new Wine();
+		
+		/*
+			Stack									Heap
+			 ___________
+			|Wine       |	  						0x2345 : Wine
+			|_ _ _ _ _ _|							0x2346 : Wine
+			|wine2      |	  
+			|_ _ _ _ _ _|
+			|0x2346     |
+			|___________|
+			|AdultBevera|	  
+			|_ _ _ _ _ _|
+			|ab         |	  
+			|_ _ _ _ _ _|
+			|0x2345     |
+			|___________|
+			
+		*/
+		
+		// Primitives in memory
+		int someInt = 4;
+		char someChar = '@';
+		boolean isSomeVar = false;
+		Object someObj = null;
+		
+		/*
+			Stack 							*****  Primitives do not have a pointer to the heap,
+			 ___________								they have an actual value where the pointer would be
+			|int        |	  
+			|_ _ _ _ _ _|
+			|someInt    |	  
+			|_ _ _ _ _ _|
+			|4          |
+			|___________|
+			|char       |	  
+			|_ _ _ _ _ _|
+			|someChar   |	  
+			|_ _ _ _ _ _|
+			|@          |
+			|___________|
+			|boolean    |	  
+			|_ _ _ _ _ _|
+			|isSomeVar  |	  
+			|_ _ _ _ _ _|
+			|false      |
+			|___________|
+			|Object     |	  
+			|_ _ _ _ _ _|
+			|someObj    |	  
+			|_ _ _ _ _ _|
+			|null       |
+			|___________|
+			
+		*/
+		
+		/*
+				Arrays in memory
+			 ___________________________
+			|     <Array Type>          |
+			|_ _ _ _ _ _ _ _ _ _ _ _ _ _|
+			|     <Variable Name>       |
+			|_ _ _ _ _ _ _ _ _ _ _ _ _ _|
+			|	  <Array Size>		    |
+			|_ _ _ _ _ _ _ _ _ _ _ _ _ _|
+			|<Pointer to start of array>|
+			|___________________________|
+			
+		*/
+			String[] myStringArray = {"Some string", "some other string", "String number 3", "Finally, one last string"};
+			/*
+				Stack												Memory
+			 _________________________
+			|   Type: String		  |			 ----------------->	0x2538 : "Some string"							// myStringArray[0]
+			|_ _ _ _ _ _ _ _ _ _ _ _ _|			|					0x2539 : "some other string"					// myStringArray[1]
+			|   Name: myStringArray   |			|					0x2540 : "String number 3"						// myStringArray[2]
+			|_ _ _ _ _ _ _ _ _ _ _ _ _|			|					0x2541 : "Finally, one last string"     		// myStringArray[3]
+			|	Size: 4				  |			|	
+			|_ _ _ _ _ _ _ _ _ _ _ _ _|			|
+			|   Pointer: 0x2538 	  |_________|
+			|_________________________|
+			
+		*/
 	}
 }
