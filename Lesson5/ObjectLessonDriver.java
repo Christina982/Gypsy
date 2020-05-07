83a82d38d0286fbb36e63c278a9955c872e70723
Jason Martinez <jmartinez059@yahoo.com>
2020-05-06 21:33:35 -0500

    objects lesson and assignment 2

---
+++ Lesson5/ObjectLessonDriver.java
@@ -0,0 +1,54 @@
+/**
+ *	Lesson 2:
+ *		complete the following exercise in the spaces provided
+ *		once completed compile and run to see if outputs are what you expected
+ */
+
+public class ObjectLessonDriver {
+	
+	public static void main(String ... args) {
+		
+		// initialize an object of type ObjectLesson, name it ol1
+		
+		// print ol1 to the screen
+
+		// declare an object of type ObjectLesson, name it ol2
+
+		// initialize an object of type String with the value "Yo Mama" and name it ol2Name
+
+		// initialize a primitive of type int to the value 47 and name it ol2Age
+
+		// assing ol2 to an instantiation of type ObjectLesson and pass the arguments ol2Name, ol2Age
+
+		// print ol2 to the screen
+
+		// assign the name "Yo Papa" to the variable ol2Name
+
+		// print ol2 to the screen
+
+		// call ol1's mutator method to SET the value of it's name to "Ponelope"
+		
+		// call ol1's mutator method to SET the value of it's age to 12
+
+		// print ol1 to the screen
+
+		// create a variable called ol1Name of type string and assign it using ol1's accessor method to GET it's name
+
+		// create as variable called ol1Age of type int and assign it using ol1's accessor method to GET it's age
+
+		String nameAndAge = "Name: " + ol1Name + ", Age: " + ol1Age;
+		// guess what the value of nameAndAge should be, type your guess between the quotes
+		String myGuess = "";
+		System.out.println(nameAndAge);
+		System.out.println(myGuess);
+		// assign the following boolean a value by calling the String.equals function to compare nameAndAge and myGuess
+		boolean myGuessWasCorrect = ;
+		System.out.println("My guess was correct: " + myGuessWasCorrect);
+		String tooMuchWhiteSpace = "          Por que, mija!!!            ";
+		// create a new string called noWhiteSpace and assign it a trimmed version of tooMuchWhiteSpace
+
+		System.out.println("*****************" + tooMuchWhiteSpace + "**********************");
+		System.out.println("*****************" + noWhiteSpace + "**********************");
+
+	}
+}
