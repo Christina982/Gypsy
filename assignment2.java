41fdaa71612d0774bd9d835d462b65b22e396f0b
Christina <clmartinez29@yahoo.com>
2020-05-07 18:56:47 -0700

    assignment2

---
+++ assignment2.java
@@ -0,0 +1,45 @@
+object objectLesson = new object("ol1");
+
+System.out.ptinln("ol1");
+
+objectLesson ol2;
+
+String ol2Name = new string("Yo Mama");
+
+int ol2Age = 47; 
+
+object objectLesson = new object("ol2Name, ol2Age");
+
+System.out.println("ol2");
+
+ol2Name = "Yo Papa";
+
+System.out.println("ol2Name");
+
+this.ol1Name = "Ponelope";
+
+this,ol1Age = 12;
+
+System.out.println("ol1");
+
+String ol1Name = return this.ol1Name;
+
+int ol1Age = return this.ol1Age;
+
+String myGuess = ("Ponelope and 12");
+
+System.out.println("myGuess");
+
+boolean myGuessWasCorrect = false;
+
+System.out.println("My guess was correct: " + myGuesswasCorrect);
+
+String tooMuchWhiteSpace = "                 Por que, mija!!!                      ";
+tooMuchWhiteSpace = tooMuchWhiteSpace.trim();
+
+String noWhiteSpace = "   Por que, mija!!!   ";
+noWhiteSpace = noWhiteSpace.trim();
+
+System.out.println(tooMuchWhiteSpace);
+System.out.println(noWhiteSpace);
+
